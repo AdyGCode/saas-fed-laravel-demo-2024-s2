718b30023eee9e263373f5d2a4cca495df9670d3
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2024-09-18 12:42:50 +0800

    init: Begin the Chirpiest Chirper Clone Ever

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChirpController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,5 +18,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/**
+ * VERB         URI                     Action      Route name
+ * GET          /chirps                 index       chirps.index
+ * POST         /chirps                 store       chirps.store
+ * GET          /chirps/{chirp}/edit    edit        chirps.edit
+ * PUT/PATCH    /chirps                 update      chirps.update
+ * DELETE       /chirps/{chirp}         destroy     chirps.destroy
+ */
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'edit', 'update', 'destroy',])
+    ->middleware(['auth',]); // TODO: Add Verified later
 
 require __DIR__.'/auth.php';
